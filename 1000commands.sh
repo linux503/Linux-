23a266426f53a82d2c1e6633733e1024761d1d43
im503 <im503@qq.com>
2022-04-17 00:40:53 +0800

    update
    4.17

--- 1000commands.sh
+++ 1000commands.sh
@@ -1,16 +1,22 @@
+1.#[清除Linux记录日志]
 #echo > /var/log/wtmp #清除登录成功的日志
+#last
 #echo > /var/log/btmp #清除登录失败的日志
+#history -c
 #echo > ./.bash_history  #清除历史history记录
-#last 
-#history -c
 #vi /root/history
-#history -c
 #history -r /root/history.txt 
 #history -cw 
-#netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n #查看当前系统连接的IP
-#w #查看当前系统连接所有用户
+2.#查看当前系统连接的IP
+#netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n 
+3.#查看当前系统连接所有用户
+#w 
 #who #查看当前用户
 #who im i 当前终端
 #pkill -kill -t pts/4  #强制踢掉某tty
-#find  / name *.so   #从/开始寻找.so的文件
-#scp root@xxx.xxx.xxx.xx:/root/Documents/Project/wwwroot/controllers/web/ocsp_lib.so  /root/sofile #scp命令传输文件
+4. #从/开始寻找.so的文件
+#find  / name *.so  
+5.#scp命令传输文件
+#scp root@xxx.xxx.xxx.xx:/root/Documents/Project/wwwroot/controllers/web/ocsp_lib.so  /root/sofile 
+6.
+
--- dbback.sh
+++ dbback.sh
@@ -8,10 +8,10 @@ if [ -e /tmp/.backup.run ];then
 fi
  
 touch /tmp/.backup.run
-mysqldump -uroot -p123.com zabbix > $TARGET/zabbix-$DUMP.sql.gz
+mysqldump -uroot -p123.com zabbix > $TARGET/zabbix-$DUMP.sql
 rm /tmp/.backup.run
  
-echo BACKUP zabbix-$DUMP.sql.gz
+echo BACKUP zabbix-$DUMP.sql
  
 #clear files before two month
 echo delete files before 10 days ago
